8b54a652bfff4c692bb8724039ec30b315505056
FlareCoding <albert.slepak@gmail.com>
2022-10-21 16:11:12 -0400

    advanced and more broad testing

--- ipc/shmem_ipc/run_benchmark.sh
+++ ipc/shmem_ipc/run_benchmark.sh
@@ -2,32 +2,57 @@
 
 LOOP_COUNT=10
 
-printf "Approach 1: Client + Un-elevated Server\n"
+printf "Approach 1: Independent client (Un-elevated)\n"
 
 i=0
 while [ $i -lt $LOOP_COUNT ]
 do
-	./server &> /dev/null &
+    taskset -c 0 ./independent_client
+	sleep 0.2
+
+    i=$(( $i + 1 ))
+done
+
+printf "\n"
+
+printf "Approach 2: Independent client (Elevated + Shortcutted)\n"
+
+i=0
+while [ $i -lt $LOOP_COUNT ]
+do
+    taskset -c 0 ./independent_client_elevated
+    sleep 0.2
+
+    i=$(( $i + 1 ))
+done
+
+printf "\n"
+printf "Approach 3: Client + Un-elevated Server\n"
+
+i=0
+while [ $i -lt $LOOP_COUNT ]
+do
+	taskset -c 0 ./server &> /dev/null &
 	server_pid=$!
 
 	sleep 0.5
-	./client	
+	taskset -c 1 ./client	
 
 	wait
 	i=$(( $i + 1 ))
 done
 
 printf "\n"
-printf "Approach 2: Client + Elevated and Shortcutted Server\n"
+printf "Approach 4: Client + Elevated + Shortcutted Server\n"
 
 i=0
 while [ $i -lt $LOOP_COUNT ]
 do
-    ./server_elevated &> /dev/null &
+    taskset -c 0 ./server_elevated &> /dev/null &
     server_pid=$!
 
     sleep 0.5
-    ./client
+    taskset -c 1 ./client
 
     wait
     i=$(( $i + 1 ))
