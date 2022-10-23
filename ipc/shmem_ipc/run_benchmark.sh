e620bcb18e405363ab8a40e09f5d61510f94cd7a
FlareCoding <albert.slepak@gmail.com>
2022-10-23 16:36:43 -0400

    improved the benchmark testing script

--- ipc/shmem_ipc/run_benchmark.sh
+++ ipc/shmem_ipc/run_benchmark.sh
@@ -1,38 +1,52 @@
 #!/bin/bash
 
-LOOP_COUNT=100
+LOOP_COUNT=1000
 
 function RunApproach1 {
+	log_file=approach1_log.txt
+
+	rm -rf $log_file
+	
 	printf "Approach 1: Independent client (Un-elevated)\n"
 
 	i=0
 	while [ $i -lt $LOOP_COUNT ]
 	do
-    	taskset -c 0 ./independent_client
+    	taskset -c 0 ./independent_client >> approach1_log.txt
 		sleep 0.2
 
     	i=$(( $i + 1 ))
+		echo -n -e '  Completed Iterations: '"$i/$LOOP_COUNT"'\r'
 	done
-	printf "\n"
+	printf "\n\n"
 }
 
 
 function RunApproach2 {
+	log_file=approach2_log.txt
+
+    rm -rf $log_file
+
 	printf "Approach 2: Independent client (Elevated + Shortcutted)\n"
 
 	i=0
 	while [ $i -lt $LOOP_COUNT ]
 	do
-    	taskset -c 0 ./independent_client_elevated
+    	taskset -c 0 ./independent_client_elevated >> $log_file
     	sleep 0.2
 
     	i=$(( $i + 1 ))
+		echo -n -e '  Completed Iterations: '"$i/$LOOP_COUNT"'\r'
 	done
 	printf "\n"
 }
 
 
 function RunApproach3 {
+	log_file=approach3_log.txt
+
+    rm -rf $log_file
+
 	printf "Approach 3: Client + Un-elevated Server\n"
 
 	i=0
@@ -42,16 +56,22 @@ function RunApproach3 {
 		server_pid=$!
 
 		sleep 0.5
-		taskset -c 1 ./client	
+		taskset -c 1 ./client >> $log_file
 
 		wait
 		i=$(( $i + 1 ))
+		echo -n -e '  Completed Iterations: '"$i/$LOOP_COUNT"'\r'
+		sleep 0.2
 	done
 	printf "\n"
 }
 
 
 function RunApproach4 {
+	log_file=approach4_log.txt
+
+    rm -rf $log_file
+
 	printf "Approach 4: Client + Elevated and Shortcutted Server\n"
 
 	i=0
@@ -61,10 +81,12 @@ function RunApproach4 {
     	server_pid=$!
 
     	sleep 0.5
-    	taskset -c 1 ./client
+    	taskset -c 1 ./client >> $log_file
 
     	wait
     	i=$(( $i + 1 ))
+		echo -n -e '  Completed Iterations: '"$i/$LOOP_COUNT"'\r'
+		sleep 0.2
 	done
 	printf "\n"
 }
@@ -78,12 +100,12 @@ printf "\t[4] Client + elevated and shortcutted Server\n\n"
 read -p "Select your choice: " test_choice
 
 if [ $test_choice == "1" ]; then
-	RunApproach1 > approach1_log.txt
+	RunApproach1
 elif [ $test_choice == "2" ]; then
-    RunApproach2 > approach2_log.txt
+    RunApproach2
 elif [ $test_choice == "3" ]; then
-    RunApproach3 > approach3_log.txt
+    RunApproach3
 elif [ $test_choice == "4" ]; then
-    RunApproach4 >approach4_log.txt
+    RunApproach4
 fi
 
