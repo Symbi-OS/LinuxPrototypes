e2bbd201691dd41a674b476c49f936c84aa5f487
FlareCoding <albert.slepak@gmail.com>
2022-10-24 13:00:14 -0400

    updated exeperiment parameters and gathered more data for 2m call iterations

--- ipc/shmem_ipc/independent_client.c
+++ ipc/shmem_ipc/independent_client.c
@@ -8,7 +8,7 @@
 #include <time.h>
 #include "LINF/sym_all.h"
 
-#define STRESS_TEST_ITERATIONS 200000
+#define STRESS_TEST_ITERATIONS 2000000
 
 typedef int(*ksys_write_t)(unsigned int fd, const char *buf, size_t count);
 
@@ -22,7 +22,6 @@ int main() {
 #ifdef ELEVATED_MODE
     // Init symbiote library and kallsymlib
     sym_lib_init();
-    sym_touch_every_page_text();
 
     // Get the address of ksys_write
     ksys_write_t my_ksys_write = (ksys_write_t)sym_get_fn_address("ksys_write");
@@ -37,7 +36,7 @@ int main() {
 	// Begin stress testing
 	for (int i = 0; i < STRESS_TEST_ITERATIONS; ++i) {
 #ifdef ELEVATED_MODE
-		my_ksys_write(logfd, "ksys_write\r", 11);
+        my_ksys_write(logfd, "ksys_write\r", 11);
 #else
 		write(logfd, "ksys_write\r", 11);
 #endif
