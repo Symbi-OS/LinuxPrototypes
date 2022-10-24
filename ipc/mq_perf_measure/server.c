5ffb780c0a6534491c1c7688d5c20f711f6ecdbf
Ke Li <likelike101101@gmail.com>
2022-10-23 20:49:29 -0400

    added two examples using message queue

---
+++ ipc/mq_perf_measure/server.c
@@ -0,0 +1,110 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include "LINF/sym_all.h"
+#include <time.h>
+
+#define STRESS_TEST_ITERATIONS 200000
+
+#define SERVER_KEY_PATHNAME "key-file"
+#define PROJECT_ID 'M'
+#define QUEUE_PERMISSIONS 0660
+
+struct message_text {
+    int qid;
+    char buf [10];
+};
+
+struct message {
+    long message_type;
+    struct message_text message_text;
+};
+
+#ifdef ELEVATED_MODE
+typedef int(*ksys_write_t)(unsigned int fd, const char *buf, size_t count);
+
+static ksys_write_t my_ksys_write = NULL;
+#endif
+
+
+int main ()
+{
+    key_t msg_queue_key;
+    int qid;
+    struct message message;
+    char *filename = "tmp.txt";
+    
+    
+    #ifdef ELEVATED_MODE
+	// Init symbiote library and kallsymlib
+	sym_lib_init();
+	// Get the adress of ksys_write
+	my_ksys_write = (ksys_write_t)sym_get_fn_address("ksys_write");
+	sym_elevate();
+
+    #endif
+
+    if ((msg_queue_key = ftok (SERVER_KEY_PATHNAME, PROJECT_ID)) == -1) {
+        perror ("ftok");
+        exit (1);
+    }
+
+    if ((qid = msgget (msg_queue_key, IPC_CREAT | QUEUE_PERMISSIONS)) == -1) {
+        perror ("msgget");
+        exit (1);
+    }
+
+    int fd = open(filename, O_WRONLY | O_APPEND);
+    if (fd == -1)
+    {
+        printf("Error opening the file %s", filename);
+        return -1;
+    }
+
+    int i = 0;
+    while (1) {
+        // read an incoming message
+        if (msgrcv (qid, &message, sizeof (struct message_text), 0, 0) == -1) {
+            perror ("msgrcv");
+            exit (1);
+        }
+	
+
+        // process message
+        int length = strlen (message.message_text.buf);
+        
+	#ifdef ELEVATED_MODE
+			my_ksys_write(fd, message.message_text.buf, length);
+	#else
+			write(fd, message.message_text.buf, length);
+	#endif
+	
+	i++;
+        if (i == STRESS_TEST_ITERATIONS+1){
+                exit(0);
+        }
+	
+	strcpy(message.message_text.buf, "1");
+        
+
+       	int client_qid = message.message_text.qid;
+       	message.message_text.qid = qid;
+
+        // send reply message to client
+        if (msgsnd (client_qid, &message, sizeof (struct message_text), 0) == -1) {  
+           perror ("msgget");
+           exit (1);	
+	}
+
+
+
+    }
+}
+
---
+++ ipc/mq_perf_no_res_measure/client.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+
+
+#define SERVER_KEY_PATHNAME "key-file"
+#define PROJECT_ID 'M'
+
+
+struct message_text {
+    int qid;
+    char buf [10];
+};
+
+struct message {
+    long message_type;
+    struct message_text message_text;
+};
+
+int main (int argc, char **argv)
+{
+    key_t server_queue_key;
+    int server_qid, myqid;
+    struct message my_message, return_message;
+    clock_t start, end;
+    double cpu_time_used;
+    
+    // create my client queue for receiving messages from server
+    if ((myqid = msgget (IPC_PRIVATE, 0660)) == -1) {
+        perror ("msgget: myqid");
+        exit (1);
+    }
+
+    if ((server_queue_key = ftok (SERVER_KEY_PATHNAME, PROJECT_ID)) == -1) {
+        perror ("ftok");
+        exit (1);
+    }
+
+    if ((server_qid = msgget (server_queue_key, 0)) == -1) {
+        perror ("msgget: server_qid");
+        exit (1);
+    }
+
+    my_message.message_type = 1;
+    my_message.message_text.qid = myqid;
+
+    // my message
+    strcpy(my_message.message_text.buf, "aaaaaaaaaa");
+    int length = 10;
+    start = clock();
+    for (int i=0; i<200000; i++) {
+        
+	if (msgsnd (server_qid, &my_message, sizeof (struct message_text), 0) == -1) {
+            perror ("client: msgsnd");
+            exit (1);
+        }
+	
+    }
+    // remove message queue
+    end = clock();
+    cpu_time_used =  (double) (end-start);
+	printf("client time used: %f   ", cpu_time_used/CLOCKS_PER_SEC);
+    
+    if (msgctl (myqid, IPC_RMID, NULL) == -1) {
+            perror ("client: msgctl");
+            exit (1);
+    }
+
+    exit (0);
+}
