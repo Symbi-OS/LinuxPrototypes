0b193164c161a7ea4389874edac184b398929dc0
FlareCoding <albert.slepak@gmail.com>
2022-11-06 21:29:05 -0500

    began abstracting away includes and ipc structs

--- ipc/shmem_ipc/client.c
+++ ipc/shmem_ipc/client.c
@@ -1,32 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <time.h>
-
-#ifdef ELEVATED_MODE
-	#include "LINF/sym_all.h"
-
-	typedef int(*ksys_write_t)(unsigned int fd, const char *buf, size_t count);
-#endif
+#include "common.h"
 
 #ifndef INDEPENDENT_CLIENT
+#include "ipc.h"
+
 static const char* BackingFileName = "sym_server_shm";
 static const int BackingFileSize = 512;
-
-#define JOB_NO_REQUEST 0
-#define JOB_REQUESTED  1
-#define JOB_COMPLETED  2
-
-typedef struct JobRequestBuffer {
-    int fd;               // File descriptor
-    int cmd;              // Job command requested by the client
-    int response;         // Response from the server
-    volatile int status;  // Flag indicating which stage the job is at
-} JobRequestBuffer_t;
 
 void stress_test(int iterations, void* shared_memory) {
 	clock_t start, end;
