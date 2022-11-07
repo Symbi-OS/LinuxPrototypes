6faab0fbfbf427d8c436d64d70bf4055941c5845
FlareCoding <albert.slepak@gmail.com>
2022-11-06 21:29:05 -0500

    began abstracting away includes and ipc structs

---
+++ ipc/shmem_ipc/ipc.h
@@ -0,0 +1,16 @@
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define JOB_NO_REQUEST 0
+#define JOB_REQUESTED  1
+#define JOB_COMPLETED  2
+
+typedef struct JobRequestBuffer {
+    int fd;               // File descriptor
+    int cmd;              // Job command requested by the client
+    int response;         // Response from the server
+	char buffer[128];	  // Command buffer
+	int buffer_len;		  // Commabd buffer length
+    volatile int status;  // Flag indicating which stage the job is at
+} JobRequestBuffer_t;
--- ipc/shmem_ipc/server.c
+++ ipc/shmem_ipc/server.c
@@ -1,29 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include "LINF/sym_all.h"
+#include "common.h"
+#include "ipc.h"
 
 static const char* BackingFileName = "sym_server_shm";
 static const int BackingFileSize = 512;
 
-#define JOB_NO_REQUEST 0
-#define JOB_REQUESTED  1
-#define JOB_COMPLETED  2
-
-typedef struct JobRequestBuffer {
-	int fd;               // File descriptor
-	int cmd;              // Job command requested by the client
-	int response;         // Response from the server
-	volatile int status;  // Flag indicating which stage the job is at
-} JobRequestBuffer_t;
-
 #ifdef ELEVATED_MODE
-typedef int(*ksys_write_t)(unsigned int fd, const char *buf, size_t count);
-
 static ksys_write_t my_ksys_write = NULL;
 #endif
 
