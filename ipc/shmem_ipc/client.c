4a39a5ec1a455c815bfa041d42dfccd136ca6460
FlareCoding <albert.slepak@gmail.com>
2022-11-12 10:19:56 -0500

    gathered more data

--- ipc/shmem_ipc/client.c
+++ ipc/shmem_ipc/client.c
@@ -2,9 +2,6 @@
 #include "ipc.h"
 
 void stress_test(int iterations, void* shared_memory) {
-	//clock_t start, end;
-	double cpu_time_used = 0;
-
     FILE* log = fopen("/dev/null", "w");
     int logfd = fileno(log);
 
@@ -19,12 +16,15 @@ void stress_test(int iterations, void* shared_memory) {
 	struct timespec tstart={0,0}, tend={0,0};
     clock_gettime(CLOCK_MONOTONIC, &tstart);
 
-    volatile int i;
-	for (i = 0; i < iterations; ++i) {
+	//struct timespec in_loop_tstart={0,0}, in_loop_tend={0,0};
+	//double* iteration_latencies = (double*)malloc(iterations * sizeof(double));
+
+	for (int i = 0; i < iterations; ++i) {
+		//clock_gettime(CLOCK_MONOTONIC, &in_loop_tstart);
+
 #ifndef INDEPENDENT_CLIENT
         // Indicate that the job was requested
-		//submit_job_request(job_buffer);
-		__sync_bool_compare_and_swap(&job_buffer->status, JOB_COMPLETED, JOB_REQUESTED);
+		submit_job_request(job_buffer);
 
 #ifdef DO_WORK
         (void) !write(logfd, "ultra_test", 10);
@@ -35,6 +35,12 @@ void stress_test(int iterations, void* shared_memory) {
 #else
         (void) !write(logfd, "ultra_test", 10);
 #endif
+		/*
+		clock_gettime(CLOCK_REALTIME, &in_loop_tend);
+		long seconds = in_loop_tend.tv_sec - in_loop_tstart.tv_sec;
+   	 	long nanoseconds = in_loop_tend.tv_nsec - in_loop_tstart.tv_nsec;
+    	double elapsed = seconds + nanoseconds * 1e-9;
+		iteration_latencies[i] = elapsed;*/
 	}
 
 	// Stop the performance timer
@@ -43,8 +49,13 @@ void stress_test(int iterations, void* shared_memory) {
     close(logfd);
 
 	// Print the results
-	cpu_time_used = ((double)tend.tv_sec + 1.0e-9*tend.tv_nsec) - ((double)tstart.tv_sec + 1.0e-9*tstart.tv_nsec);
+	double cpu_time_used = ((double)tend.tv_sec + 1.0e-9*tend.tv_nsec) - ((double)tstart.tv_sec + 1.0e-9*tstart.tv_nsec);
   	printf("Time used: %f\n", cpu_time_used);
+  	
+	/*for (int i = 0; i < iterations; i++) {
+		printf("Time used: %f\n", iteration_latencies[i]);
+	}
+	free(iteration_latencies);*/
 }
 
 int main(int argc, char** argv) {
