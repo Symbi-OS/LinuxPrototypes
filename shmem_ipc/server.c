eba3c51b1dfa7e5bc89692729ad1925594753697
Ke Li <likelike101101@gmail.com>
2022-11-15 12:53:01 -0500

    new implememtation

--- shmem_ipc/server.c
+++ shmem_ipc/server.c
@@ -1,6 +1,5 @@
 #include "common.h"
 #include "ipc.h"
-#include <pthread.h>
 #include <sys/syscall.h>
 #include <sys/ptrace.h>
 #include <errno.h>
@@ -11,10 +10,11 @@ static ksys_write_t my_ksys_write = NULL;
 
 int ITERATIONS;
 int target_logfd = -1;
+int NUM_CLIENTS = 1;
 
-void* job_buffer_thread(void* current_job_buffer){
+void workspace_thread(workspace_t* workspace){
+
 	// Prepare the job buffer
-	JobRequestBuffer_t* job_buffer = (JobRequestBuffer_t*)current_job_buffer;
 
 #ifdef ELEVATED_MODE
 	// Get the adress of ksys_write
@@ -26,10 +26,16 @@ void* job_buffer_thread(void* current_job_buffer){
 	// Begin stress testing (+2 is necessary for open and close calls)
 	for (int i = 0; i < ITERATIONS + 2; ++i) {
 		// Wait until we get the job
-        wait_for_job_request(job_buffer);
+        while (workspace->job_buffers[idx].status != JOB_REQUESTED) {
+			idx++;
+			if (idx==NUM_CLIENTS){
+				idx = 0;
+			}
+        	continue;
+        }
 
         // Process the requested command
-		switch (job_buffer->cmd) {
+		switch (workspace->job_buffers[idx].cmd) {
 		case CMD_OPEN: {
 			job_buffer->response = open(job_buffer->buffer, job_buffer->arg1, job_buffer->arg2);
 			break;
@@ -64,8 +70,10 @@ void* job_buffer_thread(void* current_job_buffer){
 	pthread_exit(NULL);
 }
 
+
+
 int main(int argc, char** argv) {
-	int num_threads = 1;
+	
 
 	if (argc < 1){
 		printf("Usage: ./<server binary> <iterations> [optional]<nthreads>\n");
@@ -73,7 +81,7 @@ int main(int argc, char** argv) {
 		ITERATIONS = atoi(argv[1]);
 	}else if(argc == 3){
 		ITERATIONS = atoi(argv[1]);
-		num_threads = atoi(argv[2]);
+		NUM_CLIENTS = atoi(argv[2]);
 	}
 
 	workspace_t* workspace = ipc_connect_server();
@@ -82,15 +90,7 @@ int main(int argc, char** argv) {
         printf("Fail to intialize server...\n");
     }
 
-	pthread_t tid[num_threads];
-	for (int j = 0; j < num_threads; j++){
-		int err = pthread_create(&(tid[j]), NULL, &job_buffer_thread, &workspace->job_buffers[j]);
-		if (err != 0) printf("can't create thread :[%s]", strerror(err));
-	}
-
-	for (int j = 0; j < num_threads; j++){
-		pthread_join(tid[j], NULL);
-	}
+	workspace_thread(workspace);
 
 	ipc_close();
 	return 0;
