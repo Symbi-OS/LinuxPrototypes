b2791d839fc0a104962977b64443aa95aabaa6bf
Ke Li <likelike101101@gmail.com>
2022-11-15 12:53:01 -0500

    new implememtation

--- shmem_ipc/server.c
+++ shmem_ipc/server.c
@@ -1,18 +1,16 @@
 #include "common.h"
 #include "ipc.h"
-#include <pthread.h>
 
 #ifdef ELEVATED_MODE
 static ksys_write_t my_ksys_write = NULL;
 #endif
 
 int ITERATIONS;
+int NUM_CLIENTS = 1;
 
-void* job_buffer_thread(void *current_job_buffer){
+void workspace_thread(workspace_t* workspace){
 
 	// Prepare the job buffer
-	JobRequestBuffer_t* job_buffer = (JobRequestBuffer_t*)current_job_buffer;
-	int resp = 1;
 
 	#ifdef ELEVATED_MODE
 	// Get the adress of ksys_write
@@ -20,20 +18,26 @@ void* job_buffer_thread(void *current_job_buffer){
 	fprintf(stderr, "ksys_write: %p\n", my_ksys_write);
 	sym_elevate();
 	#endif
+	int resp = 1;
 
 	// Create a file to write to
 	FILE* log = fopen("stress_test_log", "w");
 	int logfd = fileno(log);
+	register int idx = 0;
 
 	// Begin stress testing
 	for (int i = 0; i < ITERATIONS; ++i) {
 		// Wait until we get the job
-        while (job_buffer->status != JOB_REQUESTED) {
+        while (workspace->job_buffers[idx].status != JOB_REQUESTED) {
+			idx++;
+			if (idx==NUM_CLIENTS){
+				idx = 0;
+			}
         	continue;
         }
 
         // Process the requested command
-		switch (job_buffer->cmd) {
+		switch (workspace->job_buffers[idx].cmd) {
 		case CMD_WRITE: {
 #ifdef ELEVATED_MODE
 			if (i % 200 == 0) {
@@ -50,11 +54,11 @@ void* job_buffer_thread(void *current_job_buffer){
 		}
 
 		// Writing in a response
-		job_buffer->response = resp;
+		workspace->job_buffers[idx].response = resp;
 		++resp;
 
         // Updating the job status flag
-		job_buffer->status = JOB_COMPLETED;
+		workspace->job_buffers[idx].status = JOB_COMPLETED;
 	}
 
 	#ifdef ELEVATED_MODE
@@ -63,12 +67,12 @@ void* job_buffer_thread(void *current_job_buffer){
 
 	// Close the log file
 	fclose(log);
-	pthread_exit (NULL);
 }
+
+
 
 int main(int argc, char** argv) {
 	
-	int num_threads = 1;
 
 	if (argc < 1){
 		printf("Usage: ./<server binary> <iterations>\n");
@@ -76,7 +80,7 @@ int main(int argc, char** argv) {
 		ITERATIONS = atoi(argv[1]);
 	}else if(argc == 3){
 		ITERATIONS = atoi(argv[1]);
-		num_threads = atoi(argv[2]);
+		NUM_CLIENTS = atoi(argv[2]);
 	}
 
 	workspace_t* workspace = ipc_connect_server();
@@ -85,15 +89,7 @@ int main(int argc, char** argv) {
         printf("Fail to intialize server...\n");
     }
 
-	pthread_t tid[num_threads];
-	for (int j = 0; j < num_threads; j++){
-		int err = pthread_create(&(tid[j]), NULL, &job_buffer_thread, &workspace->job_buffers[j]);
-		if (err != 0) printf("can't create thread :[%s]", strerror(err));
-	}
-
-	for (int j = 0; j < num_threads; j++){
-		pthread_join(tid[j], NULL);
-	}
+	workspace_thread(workspace);
 
 	ipc_close();
 	return 0;
