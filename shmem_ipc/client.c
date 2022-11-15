c864390e4156f5c8fabb9a9deb666bc1cad054d4
FlareCoding <albert.slepak@gmail.com>
2022-11-15 14:41:45 -0500

    added inner-loop timer to track per-iteration latency

--- shmem_ipc/client.c
+++ shmem_ipc/client.c
@@ -1,6 +1,14 @@
 #include "common.h"
 #include "ipc.h"
 
+int64_t calc_average(int64_t* buffer, uint64_t count) {
+	int64_t sum = 0;
+	for (uint64_t i = 0; i < count; i++) {
+		sum += buffer[i];
+	}
+	return sum / count;
+}
+
 void stress_test(int iterations, JobRequestBuffer_t* job_buffer) {
 	(void)job_buffer;
 
@@ -10,12 +18,20 @@ void stress_test(int iterations, JobRequestBuffer_t* job_buffer) {
 	FILE* log = fopen("/dev/null", "w");
     int logfd = fileno(log);
 #endif
+	// Create performance timers
+	struct timespec outerTimeStart={0,0}, outerTimeEnd={0,0},
+					innerTimeStart={0,0}, innerTimeEnd={0,0};
 
-	// Start the performance timer
-	struct timespec outerTimeStart={0,0}, outerTimeEnd={0,0};
+	// Buffer to hold inner loop times
+	int64_t* innerTimesInNs = (int64_t*)malloc(iterations * sizeof(int64_t));
+
+	// Start outer performance timer
     clock_gettime(CLOCK_MONOTONIC, &outerTimeStart);
 
 	for (int i = 0; i < iterations; ++i) {
+		// Start inner performance timer
+    	clock_gettime(CLOCK_MONOTONIC, &innerTimeStart);
+
 #ifndef INDEPENDENT_CLIENT
         // Indicate that the job was requested
 		submit_job_request(job_buffer);
@@ -25,9 +41,14 @@ void stress_test(int iterations, JobRequestBuffer_t* job_buffer) {
 #else
         (void) !write(logfd, "ksys_write\r", 11);
 #endif
+
+		// Stop the inner performance timer
+    	clock_gettime(CLOCK_MONOTONIC, &innerTimeEnd);
+
+		innerTimesInNs[i] = innerTimeEnd.tv_nsec - innerTimeStart.tv_nsec;
 	}
 
-	// Stop the performance timer
+	// Stop the outer performance timer
     clock_gettime(CLOCK_MONOTONIC, &outerTimeEnd);
 
 #ifdef INDEPENDENT_CLIENT
@@ -38,7 +59,14 @@ void stress_test(int iterations, JobRequestBuffer_t* job_buffer) {
 	// Print the results
 	double cpu_time_used = ((double)outerTimeEnd.tv_sec + 1.0e-9*outerTimeEnd.tv_nsec) -
 						   ((double)outerTimeStart.tv_sec + 1.0e-9*outerTimeStart.tv_nsec);
-  	printf("Time used: %f\n", cpu_time_used);
+  	fprintf(stderr, "%f ", cpu_time_used);
+
+	// Calculate average latency for each iteration
+	int64_t avgIterationLatency = calc_average(innerTimesInNs, iterations);
+	fprintf(stderr, "%ld\n", avgIterationLatency);
+
+	// Release the timer buffer
+	free(innerTimesInNs);
 }
 
 int main(int argc, char** argv) {
@@ -49,8 +77,8 @@ int main(int argc, char** argv) {
     // Access the shared memory
 	JobRequestBuffer_t* job_buffer = ipc_get_job_buffer();
 	if (job_buffer == NULL) {
-		printf("Fail to request working job_buffer...\n");
-		return 1;
+		fprintf(stderr, "Fail to request working job_buffer...\n");
+		return -1;
 	}
 #else
     JobRequestBuffer_t* job_buffer = 0;
