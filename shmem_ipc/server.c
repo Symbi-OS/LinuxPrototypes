877e7555a2dbe006d515ac734e087efcb975aa1d
Ke Li <likelike101101@gmail.com>
2022-11-15 13:02:30 -0500

    added iteration based on clients

--- shmem_ipc/server.c
+++ shmem_ipc/server.c
@@ -24,7 +24,8 @@ void workspace_thread(workspace_t* workspace){
 #endif
 
 	// Begin stress testing (+2 is necessary for open and close calls)
-	for (int i = 0; i < ITERATIONS + 2; ++i) {
+	register int idx = 0;
+	for (int i = 0; i < (ITERATIONS + 2)*NUM_CLIENTS; ++i) {
 		// Wait until we get the job
         while (workspace->job_buffers[idx].status != JOB_REQUESTED) {
 			idx++;
@@ -60,7 +61,7 @@ void workspace_thread(workspace_t* workspace){
 		}
 
         // Updating the job status flag
-		mark_job_completed(job_buffer);
+		mark_job_completed(workspace->job_buffers[idx]);
 	}
 
 #ifdef ELEVATED_MODE
