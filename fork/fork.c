46115010feeeefbb6cc1f4c9fb134d7206aefbc3
Tommy Unger <tunger10@gmail.com>
2023-02-07 17:07:39 -0500

    Fork updates

--- fork/fork.c
+++ fork/fork.c
@@ -2,40 +2,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdint.h>
 
-#if 0
-int bench_time(int count){
-  clock_t start, end;
-  double cpu_time_used;
-  start = clock();
-  /* Do the work. */
-  int i;
-  for(i=0; i<count; i++)
-    getpid();
 
-  end = clock();
-  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-  printf("Time used: %f\n", cpu_time_used);
-
-  return 0;
-}
-#endif
-
-// Weak function print_cr3
-void print_cr3(){
-  printf("print_cr3 not implemented\n");
+uint64_t get_cr3(){
+  uint64_t my_cr3;
+  asm volatile("mov %%cr3, %0" : "=r"(my_cr3));
+  return my_cr3;
 }
 
 int main(int argc, char *argv[]){
-    // check if parent or child
-    // Print cr3
+    fprintf(stderr, "About to fork, cr3 is %lx\n", get_cr3());
 
-    if (fork() == 0){
+    int forkid = fork();
+
+    if (forkid == 0){
         // child
-        printf("Child: %d\n", getpid());
+        // print child cr3
+        fprintf(stderr, "Child CR3: %lx\n", get_cr3());
     } else {
         // parent
-        printf("Parent: %d\n", getpid());
+        fprintf(stderr, "Parent CR3: %lx\n", get_cr3());
     }
     
     return 0;
