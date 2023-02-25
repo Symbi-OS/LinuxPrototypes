89f6f648f2415a632462c93d7c174837ea8317b4
Tommy Unger <tunger10@gmail.com>
2023-02-24 19:51:16 -0500

    Not working examples

---
+++ symbiote/task_struct/task_struct.c
@@ -0,0 +1,19 @@
+
+#include <generated/asm-offsets.h>
+#include <generated/autoconf.h>
+#include <generated/bounds.h>
+#include <generated/compile.h>
+#include <generated/timeconst.h>
+#include <generated/utsrelease.h>
+
+
+#include <linux/sched.h>
+
+struct task_struct ts;
+
+// This seems to be hard becuase we're recursively trying to track down
+// all the headers under the sun.
+
+int main() {
+
+}
