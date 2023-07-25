f71e99430b8443b46b64d7b55a512f2f736a3ac9
FlareCoding <albert.slepak@gmail.com>
2023-07-25 13:12:10 -0400

    implemented the page walk from within the symbiote process without the need for a kernel module object

--- pt_walker/pt_walker.c
+++ pt_walker/pt_walker.c
@@ -1,18 +1,110 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <x86_64Constants.h>
-#include <x86_64vmm.h>
+#include <linux/sched.h>
 #include <LINF/sym_all.h>
 
 #define PAGE_SIZE        4096
 #define NUM_ENTRIES      512
-#define PAGE_MASK        ((uint64_t)(~0xFFF))
 #define PAGE_PRESENT     0x1
 
-#define PHYSICAL_PAGE_MASK 0x000FFFFFFFFFF000UL
+#define TASK_SIZE           0x00007ffffffff000
+#define PHYSICAL_PAGE_MASK  0x000FFFFFFFFFF000
+#define KERNEL_PAGE_OFFSET  0xffff888000000000
 
-#define __va(x)			((uint64_t)((unsigned long) (x) + 0xfffffc0000000000UL))
+// typedef void *(*bpf_get_current_task_t)();
+
+// void* get_current_task() {
+//     bpf_get_current_task_t gct = NULL;
+
+//     if (gct == NULL) {
+//         gct = (bpf_get_current_task_t) sym_get_fn_address("bpf_get_current_task");
+//         if (gct == NULL) {
+//             fprintf(stderr, "failed to find __fdget() \n");
+//             exit(-1);
+//         }
+//     }
+
+//     sym_elevate();
+//     return gct();
+// }
+
+struct page_index_dict {
+    uint64_t pt_lvl_4;      // Page Directory Pointer table index
+    uint64_t pt_lvl_3;      // Page Directory table index
+    uint64_t pt_lvl_2;      // Page Table index
+    uint64_t pt_lvl_1;      // Page index inside the page table
+};
+
+void vaddr_to_page_offsets(
+    uint64_t addr,
+    struct page_index_dict* dict
+) {
+    addr >>= 12;
+    dict->pt_lvl_1 = addr & 0x1ff;
+    addr >>= 9;
+    dict->pt_lvl_2 = addr & 0x1ff;
+    addr >>= 9;
+    dict->pt_lvl_3 = addr & 0x1ff;
+    addr >>= 9;
+    dict->pt_lvl_4 = addr & 0x1ff;
+}
+
+struct page_table_entry {
+    union
+    {
+        struct
+        {
+            uint64_t present                : 1;    // Must be 1, region invalid if 0.
+            uint64_t read_write             : 1;    // If 0, writes not allowed.
+            uint64_t user_supervisor        : 1;    // If 0, user-mode accesses not allowed.
+            uint64_t page_write_through     : 1;    // Determines the memory type used to access the memory.
+            uint64_t page_cache_disabled    : 1;    // Determines the memory type used to access the memory.
+            uint64_t accessed               : 1;    // If 0, this entry has not been used for translation.
+            uint64_t dirty                  : 1;    // If 0, the memory backing this page has not been written to.
+            uint64_t page_access_type       : 1;    // Determines the memory type used to access the memory.
+            uint64_t global                 : 1;    // If 1 and the PGE bit of CR4 is set, translations are global.
+            uint64_t ignored2               : 3;
+            uint64_t page_frame_number      : 36;   // The page frame number of the backing physical page.
+            uint64_t reserved               : 4;
+            uint64_t ignored3               : 7;
+            uint64_t protection_key         : 4;    // If the PKE bit of CR4 is set, determines the protection key.
+            uint64_t execute_disable        : 1;    // If 1, instruction fetches not allowed.
+        };
+        uint64_t value;
+    };
+} __attribute__((packed));
+
+struct page_table {
+    struct page_table_entry entries[512];
+} __attribute__((aligned(PAGE_SIZE)));
+
+void print_page_table_entry(struct page_table_entry* entry) {
+    printf("------ page_table_entry info ------\n");
+    printf("    present             : %i\n", (int)entry->present);
+    printf("    read_write          : %i\n", (int)entry->read_write);
+    printf("    user_supervisor     : %i\n", (int)entry->user_supervisor);
+    printf("    page_write_through  : %i\n", (int)entry->page_write_through);
+    printf("    page_cache_disabled : %i\n", (int)entry->page_cache_disabled);
+    printf("    accessed            : %i\n", (int)entry->accessed);
+    printf("    dirty               : %i\n", (int)entry->dirty);
+    printf("    page_access_type    : %i\n", (int)entry->page_access_type);
+    printf("    global              : %i\n", (int)entry->global);
+    printf("    page_frame_number   : %lx\n",(uint64_t)entry->page_frame_number);
+    printf("    protection_key      : %i\n", (int)entry->protection_key);
+    printf("    execute_disable     : %i\n", (int)entry->execute_disable);
+    printf("\n");
+}
+
+void* get_vaddr(uint64_t addr) {
+    return (void*)(addr + KERNEL_PAGE_OFFSET);
+}
+
+uint64_t read_cr3(void) {
+    uint64_t cr3_value;
+    asm volatile("mov %%cr3, %0" : "=r" (cr3_value));
+    return cr3_value;
+}
 
 uint64_t read_cr4(void) {
     uint64_t cr4_value;
@@ -25,56 +117,65 @@ int is_pcide_enabled() {
     return (cr4 >> 17) & 1;
 }
 
-extern uint64_t read_cr3(void);
-extern void kwalk(void*);
+uint64_t walk_pagetables() {
+    uint64_t pages_walked;
+    uint64_t vmpage;
+    uint64_t cr3;
+    uint64_t pml4_base;
+    struct page_index_dict idx_dict;
+    struct page_table *pml4, *pml3, *pml2, *pml1;
+    struct page_table_entry *pml4e, *pml3e, *pml2e, *pml1e;
 
-typedef void *(*bpf_get_current_task_t)();
+    for (pages_walked = 0, vmpage = 0; vmpage < 0x00007ffffffff000; vmpage += PAGE_SIZE) {
+        vaddr_to_page_offsets(vmpage, &idx_dict);
 
-void* get_current_task() {
-    bpf_get_current_task_t gct = NULL;
+        cr3 = read_cr3();
+        pml4_base = cr3 & PHYSICAL_PAGE_MASK;
 
-    if (gct == NULL) {
-        gct = (bpf_get_current_task_t) sym_get_fn_address("bpf_get_current_task");
-        if (gct == NULL) {
-            fprintf(stderr, "failed to find __fdget() \n");
-            exit(-1);
-        }
+        pml4 = (struct page_table*)get_vaddr(pml4_base);
+
+        pml4e = &pml4->entries[idx_dict.pt_lvl_4];
+        if (!pml4e->present)
+            continue;
+
+        pml3 = (struct page_table*)get_vaddr((uint64_t)pml4e->page_frame_number << 12);
+
+        pml3e = &pml3->entries[idx_dict.pt_lvl_3];
+        if (!pml3e->present)
+            continue;
+
+        pml2 = (struct page_table*)get_vaddr((uint64_t)pml3e->page_frame_number << 12);
+
+        pml2e = &pml2->entries[idx_dict.pt_lvl_2];
+        if (!pml2e->present)
+            continue;
+
+        pml1 = (struct page_table*)get_vaddr((uint64_t)pml2e->page_frame_number << 12);
+
+        pml1e = &pml1->entries[idx_dict.pt_lvl_1];
+
+        if (!pml1e->present)
+            break;
+
+        print_page_table_entry(pml1e);
+
+        ++pages_walked;
     }
 
-    sym_elevate();
-    return gct();
+    return pages_walked;
 }
 
 int main() {
     void* ksys_write = sym_get_fn_address("ksys_write");
     printf("ksys_write is at %p\n", ksys_write);
 
-    // uint64_t testAddr = (uint64_t)ksys_write;
-
-    unsigned int level;
-    struct pte* pte = sym_get_pte((uint64_t)ksys_write, &level);
-
-    sym_print_pte(pte);
-
     sym_elevate();
-
     printf("Is PCIDE Enabled: %i\n", is_pcide_enabled());
 
-    // uint64_t cr3 = read_cr3();
-    // printf("Read cr3: 0x%lx\n", cr3);
-
-    // uint64_t pml5 = __va(cr3 & PHYSICAL_PAGE_MASK);
-    // printf("pml5 base: %p\n", pml5);
-
-    // uint64_t val = *pml5;
-    // printf("pml5 value: 0x%lx\n", val);
-
-    // struct pgd* top_level_pgd = read_pgd();
-    // printf("read_pgd : 0x%lx\n", (uint64_t)top_level_pgd);
-
-    kwalk(get_current_task());
-
-    // walk_page_table();
+    uint64_t pages_walked = walk_pagetables();
+    printf("\n==============================\n");
+    printf("----- Walked %li Pages -----\n", pages_walked);
+    printf("==============================\n");
 
     sym_lower();
 
