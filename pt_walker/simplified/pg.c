f3de451af1516d707ee0e9b6dfc6f2386c7f6499
Tommy Unger <tunger10@gmail.com>
2023-08-04 16:55:35 -0400

    This works as a linker script example

--- pt_walker/simplified/pg.c
+++ pt_walker/simplified/pg.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-extern unsigned long page_offset_base;
+
+extern void * tu_main; 
+
 int main() {
-    printf("page_offset_base: %ld\n", page_offset_base);
+    // printf("page_offset_base: %ld\n", page_offset_base);
+    printf("The first bytes of text are at %p\n", main );
+    printf("The first 8 bytes are %ld\n", *(long*)main);
+
+    printf("tu_main is %p\n", &tu_main);
+    printf("The first 8 bytes are %ld\n", *(long*)&tu_main);
+
     return 0;
 }
