f6ab690859339cdc146206b477cc5f0bd35a338a
Tommy Unger <tommyu@bu.edu>
2023-08-08 16:17:14 -0500

    Slight cleanup of script

--- linuxSO/just_ld/gen_my_ld_script.sh
+++ linuxSO/just_ld/gen_my_ld_script.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Check if the user provided a file as an argument
+if [[ -z "$1" ]]; then
+    echo "Usage: ./gen_my_ld_script.sh <path to kallsyms>" >&2
+    echo "Please provide a file from where to get the symbols." >&2
+    echo "It might be the SystemMap file produced at kernel build time or perhaps /proc/kallsyms." >&2
+    exit 1
+fi
+
+# Use the provided file or fallback to /proc/kallsyms
+FILE="$1"
+if [[ ! -f "$FILE" ]]; then
+    echo "Error: File $FILE does not exist." >&2
+    exit 2
+fi
+
 echo "INCLUDE default_ld_script.ld"
 echo ""
 
@@ -13,5 +28,5 @@ BEGIN { duplicate_count = 0; }
   }
 }
 END { print "Warning: " duplicate_count " duplicate symbols found, only the first occurrence was used." > "/dev/stderr"; }
-' /proc/kallsyms
+' "$FILE"
 
